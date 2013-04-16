3e89b34300a9fb037947c28e3c9c73616e5df718
Rafael Marteleto <rmarteleto@bitbucket.org>
2013-04-16 18:15:59 -0300

    Baby Steps

--- dynacam_proj/Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
+++ dynacam_proj/Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
@@ -8,7 +8,7 @@ function Awake () {
 // Update is called once per frame
 function Update () {
 	// Get the input vector from kayboard or analog stick
-	var directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	var directionVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 	
 	if (directionVector != Vector3.zero) {
 		// Get the length of the directon vector and then normalize it
